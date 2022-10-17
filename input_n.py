954e9118c73c1be01b3c6a61773f165cd91ff3c9
Alexey <108893923+Alexey913@users.noreply.github.com>
2022-10-17 18:04:31 +0300

    Update input_n.py

--- input_n.py
+++ input_n.py
@@ -1,3 +1,6 @@
+import log
+import excep
+
 x = 0
 y = 0
 
@@ -6,22 +9,34 @@ def init(a, b):
     global y 
     x = a
     y = b
+    log.universal_logger((x,y), data_description = "Ввод данных")
+        
 
 def int_num():
-    a = int(input('Введите число 1: '))
-    b = int(input('Введите число 2: '))
-    init (a, b)
-     
-def float_num():
-    a = float(input('Введите число 1: '))
-    b = float(input('Введите число 2: '))
+    print('\nВведите число 1:')
+    a = excep.check_int()
+    print('Введите число 2:')
+    b = excep.check_int()
     init (a, b)
 
+
+def float_num():
+    print('\nВведите число 1:')
+    a = excep.check_float()
+    print('Введите число 2:')
+    b = excep.check_float()
+    init (a, b)
+
+
 def complex_num ():
-    d_1 = float(input('Введите действительную часть числа 1: '))
-    m_1 = float(input('Введите мнимую часть числа 1: '))
+    print('\nВведите действительную часть числа 1:')
+    d_1 = excep.check_float()
+    print('Введите мнимую часть числа 1:')
+    m_1 = excep.check_float()
     a = complex(d_1, m_1)
-    d_2 = float(input('Введите действительную часть числа 2: '))
-    m_2 = float(input('Введите мнимую часть числа 2: '))
+    print('\nВведите действительную часть числа 2:')
+    d_2 = excep.check_float()
+    print('Введите мнимую часть числа 2: ')
+    m_2 = excep.check_float()
     b = complex(d_2, m_2)
     init (a, b)
