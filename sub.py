7492be639f6486dc98c1754b8b31e3b208c6b018
Alexey <108893923+Alexey913@users.noreply.github.com>
2022-10-17 18:05:34 +0300

    Update sub.py

--- sub.py
+++ sub.py
@@ -1,4 +1,7 @@
 import input_n
+import log
 
 def sub():
-    return input_n.x - input_n.y
+    sub_num = input_n.x - input_n.y
+    log.universal_logger(sub_num, data_description = "Разность")    
+    return sub_num
