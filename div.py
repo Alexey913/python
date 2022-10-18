2f871ce5da7b22abc806c497168fe2516c38924c
Alexey <108893923+Alexey913@users.noreply.github.com>
2022-10-18 13:41:37 +0300

    Update div.py

--- div.py
+++ div.py
@@ -4,28 +4,18 @@ import excep
 
 
 def float_div():
-    div_float = 0
-    if excep.excep_check() is False:
-        excep.if_zero()
-    else:
-        div_float = input_n.x / input_n.y
-        log.universal_logger(div_float, data_description = "Частное")
+    div_float = input_n.x / input_n.y
+    log.universal_logger(div_float, data_description = "Частное")
     return div_float
 
 
 def floor_div():
-    if excep.excep_check() is False:
-        excep.if_zero()   
-    else:
-        div_floor = input_n.x // input_n.y
-        log.universal_logger(div_floor, data_description = "Целочисленное деление")
+    div_floor = input_n.x // input_n.y
+    log.universal_logger(div_floor, data_description = "Целочисленное деление")
     return div_floor
 
 
 def mod_div():
-    if excep.excep_check() is False:
-        excep.if_zero()
-    else:
-        div_mod = input_n.x % input_n.y
-        log.universal_logger(div_mod, data_description = "Остаток от деления")
+    div_mod = input_n.x % input_n.y
+    log.universal_logger(div_mod, data_description = "Остаток от деления")
     return div_mod
